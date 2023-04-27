76a25920fa1799fa3eb58ea2efb6778044294f8b
Matheus Gomes <matheusoutros@hotmail.com>
2023-04-27 16:36:56 -0300

    adicionando o js do projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+let a = "ai";
+let e = "enter";
+let i = "imes";
+let o = "ober";
+let u = "ufat";
+
+let entrada = document.querySelector('#entradaMensagem');
+let saida = document.querySelector('#saidaMensagem')
+
+
