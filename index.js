f50fe7cf81ea8aec6aa0b7d2e66c00669bbf8f80
Matheus Gomes <matheusoutros@hotmail.com>
2023-05-23 23:32:34 -0300

    fazendo o style do projeto primeiro

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-let a = "ai";
-let e = "enter";
-let i = "imes";
-let o = "ober";
-let u = "ufat";
-
 let entrada = document.querySelector('#entradaMensagem');
 let saida = document.querySelector('#saidaMensagem')
+let criptografar = document.querySelector('#btn-criptografar-PCRJ')
+let descriptografar = document.querySelector('#btn-descriptografar-PCRJ')
+let letraA = "a"
+let letraE = "e"
+let letraI = "i"
+let letraO = "o"
+let letraU = "u"
 
 
+let consoantes = ["a", "e", "i", "o", "u"];
+
+function codifica() {
+  for (let i = 0; i <= consoantes.length; i++) {
+    let entradaDosValores = entrada.value[i];
+    if(entradaDosValores == consoantes[i]) {
+      let substituindo = entradaDosValores.replaceAll("a", "1");
+      
+      saida.value = substituindo
+    }
+  }
+  
+}
+
+
+criptografar.onclick = codifica;
